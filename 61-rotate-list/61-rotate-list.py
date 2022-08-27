f2a0877a5f33c8cc410ae0225f64dcc1b0d92532
Alan <77854728+AlanFGC@users.noreply.github.com>
2022-08-27 03:09:35 -0400

    Time: 55 ms (59.93%), Space: 13.9 MB (31.40%) - LeetHub

--- 61-rotate-list/61-rotate-list.py
+++ 61-rotate-list/61-rotate-list.py
@@ -6,31 +6,43 @@
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         
+        # count size
         count = 0
         ptr = head
         while ptr:
             ptr = ptr.next
             count += 1
-        
+        # if size is null or size = 1
         if count == 0 or count == 1:
             return head
         
+        # mod arithmetic to reduce complexity
         k = k % count
+        if k == 0:
+            return head
         
-        for i in range(k):
-            lastElementVal = None
-            ptr = head
-            val = head.val
-            while ptr:
-                if ptr.next:
-                    ptr = ptr.next
-                    prev = ptr.val
-                    ptr.val = val
-                else:
-                    lastElementVal = val
-                    ptr = ptr.next
-                val = prev
-                
-            head.val = lastElementVal
+        # using this k we can understand that there is one 
+        # one new tail and one new head. 
+        # our job is calculate an rewire the darn thing,
+        dest = count - k -1
+        
+        ptr = head
+        index = 0
+        while index != dest:
+            index += 1
+            ptr = ptr.next
+        
+        newHead = ptr.next
+        newTail = ptr
+        
+        newTail.next = None
+        
+        ptr = newHead
+        while ptr.next:
+            ptr = ptr.next
+        
+        ptr.next = head
+        
+        return newHead
         
         return head
