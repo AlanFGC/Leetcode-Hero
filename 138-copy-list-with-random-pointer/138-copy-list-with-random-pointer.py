b2e8906042e9dd1d12706ca711f8a0c47e13e0c6
Alan <77854728+AlanFGC@users.noreply.github.com>
2022-08-27 14:44:43 -0400

    Time: 59 ms (45.13%), Space: 14.9 MB (47.31%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -12,32 +12,33 @@ class Solution:
         
         if not head:
             return head
-        # Look up table for lists and their random relations
-        posOld = {None:float(-inf)} # address: number
-        posNew = {float(-inf):None} # number : address
-        #cross reference the address and the numbers
-        ptr1 = head
-        newHead = Node(0)
-        ptr2 = newHead
-        count = 0
-        while ptr1:
-            posOld[ptr1] = count
-            posNew[count] = ptr2
-            ptr2.val = ptr1.val
-            ptr1 = ptr1.next
-            if ptr1:
-                ptr2.next = Node(0)
-                ptr2 = ptr2.next
-            else:
-                ptr2.next = None
-            count += 1
         
-        ptr1 = head
-        ptr2 = newHead
-        while ptr1:
-            ptr2.random = posNew[posOld[ptr1.random]]
-            ptr2 = ptr2.next
-            ptr1 = ptr1.next
+        ptr = head
         
+        # create an cross weaved 
+        while ptr:
+            TrueNext = ptr.next
+            ptr.next = Node(ptr.val, TrueNext, None)
+            ptr = TrueNext
+        
+        
+        newHead = head.next
+        ptr = head        
+        
+        while ptr:
+            TrueNext = ptr.next.next
+            copy = ptr.next
+            if ptr.random:
+                copy.random = ptr.random.next
+            ptr = TrueNext
+        
+        ptr = head.next
+        
+        while ptr:
+            if not ptr.next: break
+            ptr.next = ptr.next.next
+            ptr = ptr.next
+    
         return newHead
+        
             
