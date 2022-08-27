81ee231d6e08e3514077307e7bcd1b8642fc7dd4
Alan <77854728+AlanFGC@users.noreply.github.com>
2022-08-27 15:40:02 -0400

    Time: 2208 ms (29.05%), Space: 17.5 MB (86.96%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        size = len(nums)
+        results = set()
+        # it's better to use two pointer when we can for these kinds of problems
+        # binary search was good but has bugs and it's overall slower
+        # with two pointer we also need a set, neighter logn for binary
+        # is much much faster
+        for i in range(size):
+            print("\t", nums[i])
+            low = i + 1
+            high = size - 1
+            while high > low:
+                res = nums[i] + nums[low] + nums[high]
+                if res == 0:
+                    results.add((nums[i], nums[low], nums[high]))
+                    high += -1
+                    low += +1
+                elif res > 0:
+                    high = high - 1
+                elif res < 0:
+                    low = low + 1
+        
+        return results
+                
