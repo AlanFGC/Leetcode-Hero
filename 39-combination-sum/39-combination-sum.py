3eb2e822ff9b02725f4a79b6a8a5b8ea943a0c3b
Alan <77854728+AlanFGC@users.noreply.github.com>
2022-08-31 00:11:41 -0400

    Time: 153 ms (38.36%), Space: 14.2 MB (36.61%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,26 +1,22 @@
 # brutefore, dumb try
 def backtracking(index, subset, currSum, target, candidates, sets):
-    if currSum == target:
-        sets.append(subset)
-        return
-    elif currSum > target:
-        return
-    elif currSum == 0:
-        subset = []
-
-    secondSub = subset.copy()
     
-    newElement = candidates[index]
-    currSum += newElement
-    subset.append(newElement)
+    if currSum == target:
+        print(subset, currSum)
+        sets.append(subset.copy())
+        return
+    if currSum > target:
+        return
 
-    backtracking(index, subset, currSum, target, candidates, sets)
-
-
-    # Second Option: use another index
-    if index + 1 < len(candidates):
+    for i in range(index, len(candidates)):
+        newElement = candidates[i]
+        currSum += newElement
+        subset.append(newElement)
+        backtracking(i, subset, currSum, target, candidates, sets)
+        subset.pop()
         currSum -= newElement
-        backtracking(index + 1, secondSub, currSum, target, candidates, sets)
+    
+    
 
 
 
@@ -30,6 +26,5 @@ class Solution:
         index = 0
         currSum = 0
         subset = []
-        backtracking(index, subset, currSum, target, candidates, sets)
-        
+        backtracking(0, subset, currSum, target, candidates, sets)
         return sets
