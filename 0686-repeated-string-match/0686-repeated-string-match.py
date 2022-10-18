15623b4f279cac1962fa3048e16a8a9397e8fff5
Alan <77854728+AlanFGC@users.noreply.github.com>
2022-10-18 18:04:45 -0400

    Time: 676 ms (5.12%), Space: 13.9 MB (67.82%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -0,0 +1,46 @@
+import math
+import copy
+
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        A = copy.copy(a)
+        pattern = hash(b) # this will never change
+        
+            
+        # calculate the initial value for a to be >= b
+        # original size of A is found in sizeA
+        sizeA = len(a)
+        sizeB = len(b)
+        factor = 1
+        
+        if sizeA < sizeB:
+            factor = math.ceil(len(b)/len(a))
+            a = a * factor
+            if a == b:
+                return factor
+        elif sizeA == sizeB and a == b:
+            return 1
+        elif sizeA > sizeB and b in a:
+            return 1
+    
+        
+        for i in range(sizeB):
+            if b[i] not in a:
+                return -1
+        
+        # by this point len(a) >= len(b)
+        
+        while len(a) < (3 * sizeA * factor):
+            tail = 0
+            i = sizeB
+            while i < len(a)+1:
+                code = hash(a[tail:i])
+                if code == pattern:
+                    return len(a)//sizeA
+                tail += 1
+                i += 1
+            # this was the issue
+            a += A
+        
+        return -1
+        
